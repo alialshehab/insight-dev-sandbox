f6bfb3c68cc0bd9068933dbf54419438c963b55c
Ali AlShehab <alialshehab@Alis-MacBook-Pro.local>
2015-09-23 02:40:45 -0400

    producers and git ignore

---
+++ producer_2.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 14 17:29:32 2015
+
+@author: alialshehab
+"""
+
+# Kafka producer that reads the input data in a loop in order to simulate real time events
+import os
+import sys
+from kafka import KafkaClient, KeyedProducer, SimpleConsumer
+from datetime import datetime
+import time
+import json
+
+kafka = KafkaClient("54.175.48.121:9092")
+
+source_file = '/Users/alialshehab/Desktop/InsightDataEngineering/InsightProject/user_1.txt'
+
+def genData(topic):
+    producer = KeyedProducer(kafka)
+    while True:
+        ifile = open(source_file)
+        for line in ifile:
+            key = line[7]
+            producer.send(topic, key, line)
+            time.sleep(0.1)  # Creating some delay to allow proper rendering
+        ifile.close()
+
+genData("sentences")
